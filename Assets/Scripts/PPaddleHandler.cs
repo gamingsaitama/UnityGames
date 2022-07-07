4784daefec5eb2c2c5d68c65db71a0d8028d2c28
DESKTOP-VU24487\vasanth <vasanth863740@gmail.com>
2022-07-07 20:12:32 +0530

    Handled error for touch movement

--- Assets/Scripts/PPaddleHandler.cs
+++ Assets/Scripts/PPaddleHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace RecInfo.Game.Pingpong.Ball
 {
     public class PPaddleHandler : MonoBehaviour
@@ -13,7 +14,7 @@ namespace RecInfo.Game.Pingpong.Ball
         public Transform player2;
 
         private float movement;
-        private Touch touch;
+        private Touch[] touch ;
         [SerializeField] float touchspeed;
 
         private void Start()
@@ -36,22 +37,21 @@ namespace RecInfo.Game.Pingpong.Ball
                 rb.velocity = new Vector2(rb.velocity.y, movement * speed);
             }
 
-            if (Input.touchCount > 0)
+            if (Input.touches.Length > 0)
             {
-                for (int i = 0; i <= Input.touches.Length; i++)
+                foreach (Touch touch in Input.touches)
                 {
-                    touch = Input.touches[i];
+                    // Debug.Log("i value>>>> " + Input.touches.Length + i);
                     if (touch.position.x < Screen.width / 2)
                     {
                         player1.position = new Vector2(player1.position.x, player1.position.y + touch.deltaPosition.y * touchspeed * Time.deltaTime);
                     }
 
-                    if (touch.position.x > Screen.width / 2)
+                    else
                     {
                         player2.position = new Vector2(player2.position.x, player2.position.y + touch.deltaPosition.y * touchspeed * Time.deltaTime);
                     }
                 }
-
             }
         }
 
@@ -63,20 +63,6 @@ namespace RecInfo.Game.Pingpong.Ball
     }
 }
 
-        //if (Input.touchCount > 0)
-        //{
-        //    touch = Input.GetTouch(0);
-        //    if (touch.position.x < Screen.width / 2 ) 
-        //    {
-        //        player1.position = new Vector2(player1.position.x, player1.position.y + touch.deltaPosition.y * touchspeed * Time.deltaTime);
-        //    }
 
-        //    if(touch.position.x > Screen.width / 2)
-        //    {
-        //      //  touch = Input.GetTouch(0);
-        //        player2.position = new Vector2(player2.position.x, player2.position.y + touch.deltaPosition.y * touchspeed * Time.deltaTime);
-        //    }
-        //}
 
-    
 
