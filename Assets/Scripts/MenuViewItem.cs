6fb142ec457aa8eccdaf8dad6303712eaec12cd3
siva.Rec <siva.rec@ncoregames.com>
2022-07-15 00:40:29 +0530

    MenuDataAdded

--- Assets/Scripts/MenuViewItem.cs
+++ Assets/Scripts/MenuViewItem.cs
@@ -5,29 +5,34 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using TMPro;
+using UnityEngine.Networking;
+using SimpleJSON;
 
 public class MenuViewItem : MonoBehaviour
 {
-	[Serializable]
+	/*[Serializable]
 	public struct Game
 	{
 		public string Name;
 		public Sprite Icon;
-		
+
 	}
 
-	[SerializeField] Game[] allGames;
+	[SerializeField] Game[] allGames;*/
 
 	public GameObject Pubg;
 	public Transform MenuOption;
 
+	private string GameDataUrl = "https://raw.githubusercontent.com/AmDce/UnityGames/main/demoJSON.json";
+
 	void Start()
 	{
-		SetMenuData();
+		//SetMenuData();
+		StartCoroutine(SetJsonData());
 	}
 
-	private void SetMenuData()
-    {
+	/*private void SetMenuData()
+	{
 		int N = allGames.Length;
 		for (int i = 0; i < N; i++)
 		{
@@ -37,5 +42,46 @@ public class MenuViewItem : MonoBehaviour
 			//btn.GetComponent<Button>().onClick.AddListener();
 		}
 	}
+	*/
+	IEnumerator SetJsonData()
+	{
+		UnityWebRequest uwr = UnityWebRequest.Get(GameDataUrl);
+		yield return uwr.SendWebRequest();
 
+		if (uwr.result == UnityWebRequest.Result.ConnectionError)
+		{
+			Debug.Log(UnityWebRequest.Result.ConnectionError);
+		}
+		else
+		{
+			var json = JSON.Parse(uwr.downloadHandler.text);
+			JSONArray jsonObject = json["GameData"] as JSONArray;
+
+			foreach (JSONObject item in jsonObject)
+			{
+				Debug.Log($"gameid: {item["id"]}");
+				Debug.Log($"gameName: {item["gameName"]}");
+				Debug.Log($"sceneName: {item["sceneName"]}");
+				var btn = Instantiate(Pubg, MenuOption);
+				btn.GetComponent<GameButtonDataHandler>().gameName.text = item["gameName"];
+			}
+		}
+
+	}
 }
+
+public class JsonGameData
+{
+    public List<GameData> GameData;
+}
+
+public class GameData
+{
+    public string id;
+    public string gameName;
+    public string sceneName;
+    public string iconpath;
+}
+
+
+
