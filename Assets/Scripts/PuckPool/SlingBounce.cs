66e1ba54d1982847554ec7463b1a99e5b8d6be2a
Karthikeyan <karthikgamma@gmail.com>
2022-08-25 17:11:01 +0530

    Implemented multitouch functionality(can move all strikers).

--- Assets/Scripts/PuckPool/SlingBounce.cs
+++ Assets/Scripts/PuckPool/SlingBounce.cs
@@ -3,36 +3,29 @@ using UnityEngine;
 public class SlingBounce : MonoBehaviour
 {
     public float BounceForce;
-    private bool _canLaunchStriker = false;
-    private bool _canLaunchOppoStriker = false;
 
     private void OnCollisionStay2D(Collision2D collision)
-     {
-         if (collision.transform.tag == "Strikers")
-         {
-            _canLaunchStriker = true;
-            if (Input.touchCount > 0)
-            {
-                Touch _touch = Input.GetTouch(0);
-                if (_touch.phase == TouchPhase.Ended && _touch.position.y < Screen.height/2 && _canLaunchStriker) 
-                {
-                    collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * BounceForce, ForceMode2D.Impulse);
-                    _canLaunchStriker = false;
-                }
-            }
-         }
-        else if (collision.transform.tag == "OppoStrikers" )
+    {
+        if (Input.touchCount > 0)
         {
-            _canLaunchOppoStriker = true;
-            if (Input.touchCount > 0)
+            foreach (var _touch in Input.touches)
             {
-                Touch _touch = Input.GetTouch(0);
-                if (_touch.phase == TouchPhase.Ended && _touch.position.y > Screen.height / 2 && _canLaunchOppoStriker)
+                if (collision.transform.tag == "Strikers")
                 {
-                    collision.gameObject.GetComponent<Rigidbody2D>().AddForce(-Vector2.up * BounceForce, ForceMode2D.Impulse);
-                    _canLaunchOppoStriker = false;
+                    if (_touch.phase == TouchPhase.Ended && _touch.position.y < Screen.height / 2)
+                    {
+                        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * BounceForce, ForceMode2D.Impulse);
+                    }
+                }
+                else if (collision.transform.tag == "OppoStrikers")
+                {
+                    if (_touch.phase == TouchPhase.Ended && _touch.position.y > Screen.height / 2)
+                    {
+                        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(-Vector2.up * BounceForce, ForceMode2D.Impulse);
+                    }
                 }
             }
+
         }
-     }
+    }
 }
