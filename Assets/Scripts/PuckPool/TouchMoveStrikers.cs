28172b2d056adb007f36ef9985dfb40189fd5d6f
Karthikeyan <karthikgamma@gmail.com>
2022-08-26 21:01:09 +0530

    Handled touch canceled exceptions.

--- Assets/Scripts/PuckPool/TouchMoveStrikers.cs
+++ Assets/Scripts/PuckPool/TouchMoveStrikers.cs
@@ -39,28 +39,34 @@ public class TouchMoveStrikers : MonoBehaviour
                         TouchObjects touchObj = _touchObjects.Find(touch => touch.fingerID == t.fingerId);
                         TouchObjects opptouchObj = _opptouchObjects.Find(touch => touch.fingerID == t.fingerId);
 
-                        if (touchObj != null && touchObj.selectedItem.transform.position.y < -0.4 && touchObj.selectedItem.CompareTag("Strikers"))//_touchObjects.Count > 0 &&
+                        if (touchObj != null && touchObj.selectedItem.transform.position.y < -0.4 && touchObj.selectedItem.CompareTag("Strikers"))
                         {
                             touchObj.selectedItem.transform.position = new Vector2(Mathf.Clamp(touches[t.fingerId].x, -1.78f, 1.78f),
                                                             Mathf.Clamp(touches[t.fingerId].y, -3.54f, -0.6f));
                         }
-                        else if (_opptouchObjects != null && opptouchObj.selectedItem.transform.position.y > 0.4 && opptouchObj.selectedItem.CompareTag("OppoStrikers"))//_opptouchObjects.Count > 0 && 
+                        else if (opptouchObj != null && opptouchObj.selectedItem.transform.position.y > 0.4 && opptouchObj.selectedItem.CompareTag("OppoStrikers"))
                         {
                             opptouchObj.selectedItem.transform.position = new Vector2(Mathf.Clamp(touches[t.fingerId].x, -1.78f, 1.78f),
                                                        Mathf.Clamp(touches[t.fingerId].y, 0.6f, 3.54f));
                         }
                     }
-                    else if (Input.GetTouch(t.fingerId).phase == TouchPhase.Ended)
+                    else if (Input.GetTouch(t.fingerId).phase == TouchPhase.Ended || Input.GetTouch(t.fingerId).phase == TouchPhase.Canceled)
                     {
                         if (_touchObjects.Count > 0)
                         {
                             TouchObjects touchObj = _touchObjects.Find(touch => touch.fingerID == t.fingerId);
-                            _touchObjects.RemoveAt(_touchObjects.IndexOf(touchObj));
+                            if (touchObj != null)
+                            {
+                                _touchObjects.RemoveAt(_touchObjects.IndexOf(touchObj));
+                            }
                         }
                         else if (_opptouchObjects.Count > 0)
                         {
                             TouchObjects opptouchObj = _opptouchObjects.Find(touch => touch.fingerID == t.fingerId);
-                            _opptouchObjects.RemoveAt(_opptouchObjects.IndexOf(opptouchObj));
+                            if (opptouchObj != null)
+                            {
+                                _opptouchObjects.RemoveAt(_opptouchObjects.IndexOf(opptouchObj));
+                            }
                         }
                         _touchObjects.Clear();
                         _opptouchObjects.Clear();
