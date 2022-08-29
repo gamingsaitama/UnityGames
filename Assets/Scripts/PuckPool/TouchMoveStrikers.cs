5e311c89343327de123afb7fef34bebdb9732e1a
Karthikeyan <karthikgamma@gmail.com>
2022-08-29 19:57:12 +0530

    Exception handled related to sling bounce and touchmove.

--- Assets/Scripts/PuckPool/TouchMoveStrikers.cs
+++ Assets/Scripts/PuckPool/TouchMoveStrikers.cs
@@ -39,12 +39,12 @@ public class TouchMoveStrikers : MonoBehaviour
                         TouchObjects touchObj = _touchObjects.Find(touch => touch.fingerID == t.fingerId);
                         TouchObjects opptouchObj = _opptouchObjects.Find(touch => touch.fingerID == t.fingerId);
 
-                        if (touchObj != null && touchObj.selectedItem.transform.position.y < -0.4 && touchObj.selectedItem.CompareTag("Strikers"))
+                        if (touchObj != null && touchObj.selectedItem.transform.position.y < -0.1 && touchObj.selectedItem.CompareTag("Strikers"))
                         {
                             touchObj.selectedItem.transform.position = new Vector2(Mathf.Clamp(touches[t.fingerId].x, -1.78f, 1.78f),
                                                             Mathf.Clamp(touches[t.fingerId].y, -3.54f, -0.6f));
                         }
-                        else if (opptouchObj != null && opptouchObj.selectedItem.transform.position.y > 0.4 && opptouchObj.selectedItem.CompareTag("OppoStrikers"))
+                        else if (opptouchObj != null && opptouchObj.selectedItem.transform.position.y > 0.1 && opptouchObj.selectedItem.CompareTag("OppoStrikers"))
                         {
                             opptouchObj.selectedItem.transform.position = new Vector2(Mathf.Clamp(touches[t.fingerId].x, -1.78f, 1.78f),
                                                        Mathf.Clamp(touches[t.fingerId].y, 0.6f, 3.54f));
@@ -52,26 +52,25 @@ public class TouchMoveStrikers : MonoBehaviour
                     }
                     else if (Input.GetTouch(t.fingerId).phase == TouchPhase.Ended || Input.GetTouch(t.fingerId).phase == TouchPhase.Canceled)
                     {
-                        if (_touchObjects.Count > 0)
+                        TouchObjects touchObj = _touchObjects.Find(touch => touch.fingerID == t.fingerId);
+                        if (touchObj != null)
                         {
-                            TouchObjects touchObj = _touchObjects.Find(touch => touch.fingerID == t.fingerId);
-                            if (touchObj != null)
-                            {
-                                _touchObjects.RemoveAt(_touchObjects.IndexOf(touchObj));
-                            }
+                            _touchObjects.RemoveAt(_touchObjects.IndexOf(touchObj));
                         }
-                        else if (_opptouchObjects.Count > 0)
+                        TouchObjects opptouchObj = _opptouchObjects.Find(touch => touch.fingerID == t.fingerId);
+                        if (opptouchObj != null)
                         {
-                            TouchObjects opptouchObj = _opptouchObjects.Find(touch => touch.fingerID == t.fingerId);
-                            if (opptouchObj != null)
-                            {
-                                _opptouchObjects.RemoveAt(_opptouchObjects.IndexOf(opptouchObj));
-                            }
+                            _opptouchObjects.RemoveAt(_opptouchObjects.IndexOf(opptouchObj));
                         }
-                        _touchObjects.Clear();
-                        _opptouchObjects.Clear();
+
                     }
                 }
+            }
+            else
+            {
+                _touchObjects.Clear();
+                _opptouchObjects.Clear();
+                touches = new Vector2[10];
             }
         }
         catch (Exception ex)
@@ -79,6 +78,7 @@ public class TouchMoveStrikers : MonoBehaviour
             Debug.Log(ex.Message);
         }
     }
+
     [Serializable]
     public class TouchObjects
     {
